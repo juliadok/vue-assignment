bf1c270c5dfd0cb8992dc0c8365d86b28f645f97
Julia Dokolina <juliadokolina@gmail.com>
2020-10-23 21:39:00 +0200

    Add Articles component

---
+++ src/components/Articles.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container pt-4">
+    <Title :articles="articles" />
+    <Article
+      v-for="article in articles.bundelItems"
+      :key="article.id"
+      :article="article"
+    />
+  </div>
+</template>
+
+<script>
+import Title from './Title.vue';
+import Article from './Article.vue';
+
+export default {
+  name: 'Articles',
+  components: {
+    Title,
+    Article,
+  },
+  props: {
+    articles: Object,
+  },
+};
+</script>
