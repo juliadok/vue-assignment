56ffc8b495fcd6b147fcc5a27cc124d884e37fe0
Julia Dokolina <juliadokolina@gmail.com>
2020-10-23 21:37:43 +0200

    Add Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav class="navbar navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand text-uppercase font-weight-bold" href="https://www.rtlboulevard.nl/">Vue Boulevard</a>
+      </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+};
+</script>
